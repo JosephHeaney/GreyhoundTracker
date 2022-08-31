b436a5b35e10bcb7b88844031eb48386c3f4a02f
JosephHeaney <49104704+JosephHeaney@users.noreply.github.com>
2022-08-31 12:00:09 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,11 @@ const App = () => {
             <Container maxWidth="xl">
                 <PrimarySearchAppBar />
                     <Switch>
-                        <Route path="/" exact component={Home} />
                         <Route path="/login" exact component={Login} />
+                        <Route path="/" exact component={Home} />
+                            //if the home screen shows up first instead of the login screen then change the home screen path to "/home" and the login screen to "/"
+                            //I should have this done with an authentication that it checks if you're logged in before bringing you to the home screen but I assume i
+                            //didn't have time for that 
                     </Switch>
             </Container>
         </BrowserRouter>
